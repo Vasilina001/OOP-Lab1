a1e3fad07108cbbf53e13cf2a9b42f2af18af54c
Vasilina001 <vasilinamatviisin@gmail.com>
2025-09-14 22:20:52 +0300

    A simple console application were created

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-﻿// See https://aka.ms/new-console-template for more information
-namespace ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 5;
-            int y = 10;
-            Console.WriteLine("5 + 10 = {0}", x + y);
-            Console.ReadKey();
-        }
-    }
-}
-=======
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.ReadKey();
->>>>>>> d9be1b3234570addd0a38552cd277901a51a86e5
